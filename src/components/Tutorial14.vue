93d36692af121d667188736fc8d1234e8652a3a4
arakaki hayate <hayateasdf@gmail.com>
2024-07-05 10:07:51 +0900

    すべてのチュートリアルは一応終了

---
+++ src/components/Tutorial14.vue
@@ -0,0 +1,23 @@
+<script setup>
+/**
+ * https://ja.vuejs.org/tutorial/#step-14
+ * スロット
+ * 
+ * 親コンポーネントから子コンポーネントへ渡すこと
+ * 
+ */
+import { ref } from 'vue'
+import Comp from './Tutorial14ChildComp.vue'
+const msg = ref('from yet')
+</script>
+
+<template>
+    <!-- 親コンポーネントから子に渡す1 -->
+    <Comp>1. Message: {{ msg }}</Comp>
+
+    <!-- 親コンポーネントから子に渡す2 -->
+    <Comp>2. World World World</Comp>
+
+    <!-- 親コンポーネントから渡さない場合 -->
+    <Comp></Comp>
+</template>
---
+++ src/components/Tutorial14ChildComp.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <!-- 親コンポーネントからのコンテンツをレンダリング(slot)、ない場合、デフォルトの値 -->
+        <slot>Fallback content</slot>
+    </div>
+</template>
