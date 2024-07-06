3567eef97ae79b1ce614379d7220a49872352ae0
arakaki hayate <hayateasdf@gmail.com>
2024-07-06 10:01:39 +0900

    2columnのサンプルから今まで作成したチュートリアルを切り替えて表示するまで作成(仮)

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,41 @@
 
 <script setup>
-  // import Tutorial1 from './components/Tutorial1.vue'
-  import Tutorial from './components/Tutorials/Tutorial14.vue'
+import { shallowRef, ref } from 'vue'
+import Sidebar from './components/2Column/Sidebar.vue'
+// import Main from './components/2Column/Main.vue'
+import Tutorial1 from './components/Tutorials/Tutorial1.vue'
+import Tutorial2 from './components/Tutorials/Tutorial2.vue'
+import Tutorial3 from './components/Tutorials/Tutorial3.vue'
+import Tutorial13 from './components/Tutorials/Tutorial13.vue'
+import Tutorial14 from './components/Tutorials/Tutorial14.vue'
+
+const compList = [
+  Tutorial1,
+  Tutorial2,
+  Tutorial3,
+  Tutorial13,
+  Tutorial14,
+]
+// TODO: refからshallowRefに変えるとエラーが出なくなった。後で調べる
+const currentComp = shallowRef(Tutorial1);
+
+function sideResponse(index) {
+  currentComp.value = compList[index];
+}
+
+const names = compList.map(x => x.__name);
+// console.log(names)
 </script>
-
-<style scoped>
-  button {
-    width: 150px;
-    height: 30px;
-  }
-  .wrapper {
-    font-size: 30px;
-  }
-  a.focus {
-    background-color: lightblue;
-  }
-</style>
-
-<template>
-  <div id="app">
-    <!-- <ChildComponent></ChildComponent> -->
-     <Tutorial></Tutorial>
-  </div>
-</template>
 
 <style scoped>
 .container {
   display: flex;
-  height: 100vh;
+    height: 100vh;
+  }
+
+.main {
+    overflow: auto;
+    flex: 2;
 }
 
 .sidebar {
@@ -39,12 +47,22 @@
   overflow: auto;
 }
 
-.main {
-  overflow: auto;
-  flex: 2;
-}
-
-.box {
-  height: 150vh;
-}
 </style>
+
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="sidebar">
+        <Sidebar 
+          v-bind:names="names"
+          v-on:response="sideResponse"></Sidebar>
+      </div>
+
+      <div class="main">
+        <!-- <KeepAlive></KeepAlive> -->
+        <component :is="currentComp"></component>
+        <!-- <Main></Main> -->
+      </div>
+    </div>
+  </div>
+</template>
