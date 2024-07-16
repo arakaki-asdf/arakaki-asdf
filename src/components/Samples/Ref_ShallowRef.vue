bdeda30a43f0a12d6a0892112575eebbb5413cde
arakaki hayate <hayateasdf@gmail.com>
2024-07-16 10:31:02 +0900

    コミットしてなかった

--- src/components/Samples/Ref_ShallowRef.vue
+++ src/components/Samples/Ref_ShallowRef.vue
@@ -4,7 +4,7 @@
  * どれも追跡できない
  */
 import { shallowRef, watch, watchEffect } from 'vue'
-const data  = shallowRef({ text: "", id: 0 })
+const data = shallowRef({ text: "", id: 0 })
 
 watch (data, () => {
     console.log(`watch : ${data.value.text}`)
@@ -17,9 +17,15 @@ watch (() => data.value.text, (text) => {
 watchEffect(() => {
     console.log(`watchEffect : ${data.value.text}`)
 })
+
+// プロパティではなく、オブジェクトの変更なら上記すべてで追跡されている
+function changeButton() {
+    data.value = { text: "change button", id: 100 }
+}
 </script>
 
 <template>
     <input type="text" v-model="data.text" placeholder="input...">
     <p>text: {{ data.text }}</p>
+    <button v-on:click="changeButton">changeButton</button>
 </template>
